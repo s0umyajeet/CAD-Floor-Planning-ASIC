d007919e45d8c8833ec5f8197f408b09c8d2a27c
Soumyajeet <mandal.soumyajeet@gmail.com>
2021-09-09 11:58:02 +0530

    Shape class for handling all shape data

---
+++ Shape.cpp
@@ -0,0 +1,42 @@
+#include "Shape.h"
+#include <iostream>
+
+
+Shape::Shape() {
+	cmd = "NULL";
+	id = "NULL";
+	area = INF;
+	for (auto& x : color) { x = INF; }
+	len_in_x = INF;
+	len_in_y = INF;
+	offsetX = INF;
+	offsetY = INF;
+	posX = INF;
+	posY = INF;
+	ref_shape_id = "NULL";
+}
+
+
+void Shape::show_data(std::stringstream &outputss) {
+	outputss << "\t" << "cmd " << this->cmd << std::endl;
+	outputss << "\t" << "id " << this->id << std::endl;
+	outputss << "\t" << "area " << this->area << std::endl;
+	outputss << "\t" << "len_in_x " << this->len_in_x << std::endl;
+	outputss << "\t" << "len_in_y " << this->len_in_y << std::endl;
+	outputss << "\t" << "offsetX " << this->offsetX << std::endl;
+	outputss << "\t" << "offsetY " << this->offsetY<< std::endl;
+	outputss << "\t" << "posX " << this->posX << std::endl;
+	outputss << "\t" << "posY " << this->posY << std::endl;
+	outputss << "\t" << "ref_shape_id " << this->ref_shape_id << std::endl;
+}
+
+
+
+/*
+shape::shape(shape&& other)
+	: cmd(std::move(other.cmd)), id(std::move(other.id)), area(std::move(other.area)), len_in_x(std::move(other.len_in_x)), len_in_y(std::move(other.len_in_y)),
+	offsetx(std::move(other.offsetx)), offsety(std::move(other.offsety)), posx(std::move(other.posx)), posy(std::move(other.posy)), ref_shape_id(std::move(other.ref_shape_id))
+{
+
+}
+*/
---
+++ Shape.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <string>
+#include <sstream>
+
+constexpr auto INF = 99999999999;
+
+class Shape
+{
+private:
+	std::string cmd;
+	std::string id;
+	int area;
+	int color[4];
+
+	//dimensions
+	int len_in_x;
+	int len_in_y;
+
+	//offsets for lower left vertex
+	int offsetX;
+	int offsetY;
+
+	//actual position of lower left vertex;
+	int posX;
+	int posY;
+
+	//id reference shape 
+	std::string ref_shape_id;
+
+	//Shape(Shape&& other);
+	//Shape&& operator=(Shape&& other);
+
+public:
+	Shape();
+	//Shape(std::string cmd, std::string id, int area, int color[], int offsetX, int offsetY, std::string ref_shape);
+
+	//Shape(const )
+	void show_data(std::stringstream &outputss);
+	void setArea(int area) { this->area = area; }
+	void setposX(int x) { this->posX = x; }
+	void setposY(int y) { this->posY = y; }
+	void setID(std::string id) { this->id = id; }
+	void setcmd(std::string cmd) { this->cmd = cmd; }
+	void set_ref_shape_id(std::string ref) { this->ref_shape_id = ref; }
+	void set_offset_x(int offsetX) { this->offsetX = offsetX; }
+	void set_offset_y(int offsetY) { this->offsetY = offsetY; }
+	void set_len_in_x(int len) { this->len_in_x = len; }
+	void set_len_in_y(int len) { this->len_in_y = len; }
+
+	const inline int getArea() { return this->area; }
+	const inline int getposX() { return this->posX; }
+	const inline int getposY() { return this->posY; }
+	const inline int getoffsetX() { return this->offsetX; }
+	const inline int getoffsetY() { return this->offsetY; }
+	const inline int get_len_in_x() { return this->len_in_x * 25; }
+	const inline int get_len_in_y() { return this->len_in_y * 25; }
+
+	inline const std::string getID() { return this->id; }
+	inline const std::string get_ref_id() { return this->ref_shape_id; }
+};
+
