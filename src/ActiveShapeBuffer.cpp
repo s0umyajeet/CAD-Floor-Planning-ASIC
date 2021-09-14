df9a8c18a19e8bb24b11df14d6c59a962c82331b
Soumyajeet <mandal.soumyajeet@gmail.com>
2021-09-14 23:25:50 +0530

    updated source files

---
+++ src/ActiveShapeBuffer.cpp
@@ -0,0 +1,16 @@
+#include "ActiveShapeBuffer.h"
+
+ActiveShapeBuffer& ActiveShapeBuffer::get()
+{
+	static ActiveShapeBuffer instance;
+	return instance;
+}
+
+std::map<std::string, Shape> ActiveShapeBuffer::shapePlacementMap;
+std::map<std::string, int> ActiveShapeBuffer::shapeAreaMap;
+
+rapidcsv::Document ActiveShapeBuffer::areaFile = rapidcsv::Document("E:/Dev/C++ Projects/iHack-CAD-2021/iHack-CAD-2021/src/shapes.csv", rapidcsv::LabelParams(-1, -1)); // , rapidcsv::SeparatorParams(' '));
+rapidcsv::Document ActiveShapeBuffer::placementFile = rapidcsv::Document("E:/Dev/C++ Projects/iHack-CAD-2021/iHack-CAD-2021/src/placement.csv", rapidcsv::LabelParams(-1, -1), rapidcsv::SeparatorParams(' '));
+
+
+ActiveShapeBuffer::ActiveShapeBuffer() {}
